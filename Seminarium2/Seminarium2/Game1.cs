9a3c367deb1e70012a5d318806523911bbbad24d
Jonatan Karlsson <JK98@Live.se>
2020-12-10 10:39:34 +0100

    Revert "Cleared out some junk from game1"
    This reverts commit 4a84e71de4c5f5b98e86af7a4751c8d7b5cecfed.

--- Seminarium2/Seminarium2/Game1.cs
+++ Seminarium2/Seminarium2/Game1.cs
@@ -18,16 +18,22 @@ namespace Seminarium2
 
         protected override void Initialize()
         {
+            // TODO: Add your initialization logic here
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
         }
 
         protected override void UnloadContent()
         {
+            // TODO: Unload any non ContentManager content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,6 +41,7 @@ namespace Seminarium2
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
@@ -42,6 +49,8 @@ namespace Seminarium2
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
