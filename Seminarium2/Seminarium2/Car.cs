b3d4d78e98ea70fb90b6fe07742f967e82f442db
aswx12 <75431445+aswx12@users.noreply.github.com>
2020-12-10 10:33:06 +0100

    as

--- Seminarium2/Seminarium2/Car.cs
+++ Seminarium2/Seminarium2/Car.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,38 @@ namespace Seminarium2
 {
     class Car
     {
+        Texture2D car;
+        Vector2 position;
+
+        Vector2 carOrigin;
+        Vector2 velocity;
+
+        float carRotation = 0;
+
+        int speed;
+
+        public Car(Texture2D car, Vector2 position, Vector2 velocity)
+        {
+            this.car = car;
+
+            this.position = position;
+            this.velocity = velocity;
+
+            carOrigin = new Vector2(car.Width / 2, car.Height / 2);
+  
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            speed = 5;
+            carRotation += (float)gameTime.ElapsedGameTime.TotalSeconds * speed; //?
+            position += velocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+       
+            spriteBatch.Draw(car, position, null, Color.White, carRotation, carOrigin, 1, SpriteEffects.None, 0);
+        }
     }
 }
