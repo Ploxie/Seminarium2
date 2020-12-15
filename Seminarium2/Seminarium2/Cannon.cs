51ff122ffd4958dde556524448f3352f631456ad
Jonatan Karlsson <JK98@Live.se>
2020-12-15 14:36:44 +0100

    Edited the cannon
    Will need the clear to work in game 1  without fear of conflicts in order to continue

--- Seminarium2/Seminarium2/Cannon.cs
+++ Seminarium2/Seminarium2/Cannon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,23 @@ namespace Seminarium2
 {
     class Cannon
     {
+        MouseState oldMouseState, currentMouseState;
+
         public Cannon()
         {
 
         }
+
+        public void Update()
+        {
+            oldMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+
+            if(oldMouseState.LeftButton==ButtonState.Released && currentMouseState.LeftButton==ButtonState.Pressed)
+            {
+
+            }
+        }
+
     }
 }
